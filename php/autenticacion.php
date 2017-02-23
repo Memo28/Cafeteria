46f97c3e5454fb9379278c69c22192f2d0accbb7
Memo <memovdg@gmail.com>
2017-02-22 23:49:54 -0600

    Log In y Log out de Empleado y Administrador Completo

---
+++ php/autenticacion.php
@@ -0,0 +1,31 @@
+<?php
+	include ("/conexion.php");
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+
+	//Coneccion y consulta a la BD
+	$link = Conectarse();
+	$email = mysqli_real_escape_string($link,$email);
+	$pass = mysqli_real_escape_string($link,$password);
+	$query = "SELECT * FROM usuario WHERE email= '$email' AND password= '$password'";
+	$run_user = mysqli_query($link, $query);
+	$check_user = mysqli_num_rows($run_user);
+
+	//Verificacion de Usuario
+
+	if($check_user>0){
+		while ($row=mysqli_fetch_row($run_user)) {
+			$tipo = $row[3];
+		}
+	}
+	// Tipo 0 son Administradores
+	//Tipo 1 son Empleados
+	if ($tipo == 0) {
+		$_SESSION[‘email’]=$email;
+		echo header("Location:../Principal_Administrador.html");
+	}else{
+		$_SESSION[‘email’]=$email;
+		echo header("Location:../Principal_Empleado#php/tables_datatables.html");
+	}
+
+?>
--- php/conexion.php
+++ php/conexion.php
@@ -1,32 +1,16 @@
 <?php
-// datos para la conexion a mysql
-$hostname = 'localhost';
-$database = 'cafe_fcc';
-$username = 'root';
-$password = '';
-$mysqli = new mysqli($hostname, $username,$password, $database);
-if ($mysqli -> connect_errno) {
-die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
-. ") " . $mysqli -> mysqli_connect_error());
-}
-else{
-	$email = $_POST["email"];
-	$password = $_POST["password"];
-
-	$email = mysqli_real_escape_string($mysqli,$email);
-	$pass = mysqli_real_escape_string($mysqli,$password);
-
-	$query = "SELECT * FROM usuario WHERE email= '$email' AND password= '$password'";
-
-	$run_user = mysqli_query($mysqli, $query);
-	$check_user = mysqli_num_rows($run_user);
-
-	if($check_user>0){
-		$_SESSION[‘email’]=$email;
-		echo header("Location:../Principal.html");
-	}else{
-		echo "<script>alert('¡Email o Contraseña incorrecta!')</script>";
-		echo "<script>location.href='../index.html'</script>";
+function Conectarse(){
+	// datos para la conexion a mysql
+	$hostname = 'localhost';
+	$database = 'cafe_fcc';
+	$username = 'root';
+	$password = '';	
+	$mysqli = new mysqli($hostname, $username,$password, $database);
+	if ($mysqli -> connect_errno) {
+	die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
+	. ") " . $mysqli -> mysqli_connect_error());
+	exit();
 	}
+	return $mysqli;
 }
 ?>
