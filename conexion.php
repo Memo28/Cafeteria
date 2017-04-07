eb3f480d693f8c6caa34e5eee607e6c24991ff3c
Memo <memovdg@gmail.com>
2017-04-07 01:53:32 -0500

    Agrego la funcion de ranking con estrellas y mostrar las sucursales

--- conexion.php
+++ conexion.php
@@ -1,11 +1,16 @@
-<?php
-// datos para la conexion a mysql
-$conexion = mysqli_connect(“localhost”,”root”,””,”users”);
-
-if (mysqli_connect_errno())
-{
-
-echo “MySQLi Connection was not established: ” . mysqli_connect_error();
-
+﻿<?php
+function Conectarse(){
+	// datos para la conexion a mysql
+	$hostname = 'mysql.hostinger.mx';
+	$database = 'u144772454_cafe';
+	$username = 'u144772454_root';
+	$password = 'memovdg';	
+	$mysqli = new mysqli($hostname, $username,$password, $database);
+	if ($mysqli -> connect_errno) {
+	die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
+	. ") " . $mysqli -> mysqli_connect_error());
+	exit();
+	}
+	return $mysqli;
 }
 ?>
